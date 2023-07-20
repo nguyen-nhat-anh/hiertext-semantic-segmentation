22822b4b6e1a0c0041c9ca1cc71813a35ce4aa8a
nguyen-nhat-anh <nguyennhatanh1995@gmail.com>
2023-07-20 04:46:44 +0000

    Update logging metric

--- src/trainer.py
+++ src/trainer.py
@@ -3,7 +3,7 @@ import segmentation_models_pytorch as smp
 import torch
 from torch.utils.data import DataLoader
 from tqdm.auto import tqdm
-from typing import List
+from typing import List, Dict
 
 from src.callback import Callbacks, Callback
 from src.dataset import DataBatch, HierTextDataModule
@@ -11,6 +11,8 @@ from src.utils import MeterDict, logger
 
 
 class HierTextModelModule:
+    categories = ["paragraph", "line", "word"]
+
     def __init__(self, encoder_name, device, lr, amp, batch_size, epochs, **kwargs):
         self.device = (
             torch.device(f"cuda:{device}")
@@ -21,7 +23,7 @@ class HierTextModelModule:
             encoder_name=encoder_name,
             encoder_weights="imagenet",
             in_channels=3,
-            classes=3,
+            classes=len(self.categories),
         ).to(self.device)
         self.lr = lr
         self.amp = amp if torch.cuda.is_available() else False
@@ -56,27 +58,28 @@ class HierTextModelModule:
             enabled=self.amp
         )  # for mixed precision training
 
-    def training_step(self, batch: DataBatch, batch_idx) -> torch.Tensor:
+    def training_step(self, batch: DataBatch, batch_idx) -> Dict[str, torch.Tensor]:
         images = batch["image"].to(self.device)
         targets = batch["mask"].to(self.device)
         with torch.cuda.amp.autocast(enabled=self.amp):
             outputs = self.model(images)
             loss = self.loss_fn(outputs, targets)
-        return loss
+        return dict(outputs=outputs, loss=loss, images=images, targets=targets)
 
-    def validation_step(self, batch: DataBatch, batch_idx) -> torch.Tensor:
+    def validation_step(self, batch: DataBatch, batch_idx) -> Dict[str, torch.Tensor]:
         images = batch["image"].to(self.device)
         targets = batch["mask"].to(self.device)
         outputs = self.model(images)
         loss = self.loss_fn(outputs, targets)
-        return loss
+        return dict(outputs=outputs, loss=loss, images=images, targets=targets)
 
     def training_epoch(self, loader: DataLoader):
         self.model.train()
-        summary_loss = MeterDict()
+        summaries = MeterDict()
         pbar = tqdm(enumerate(loader), total=len(loader))
         for step, batch in pbar:
-            loss = self.training_step(batch, step)
+            result = self.training_step(batch, step)
+            loss, targets = result["loss"], result["targets"]
             # backpropagation
             self.optimizer.zero_grad()
             if self.amp:
@@ -90,24 +93,41 @@ class HierTextModelModule:
             if self.scheduler is not None:
                 self.scheduler.step()
             # update stats
-            summary_loss.update("loss", loss.item())
+            batch_size = targets.size(0)
+            summaries.update("loss", loss.item(), batch_size)
             lr = self.optimizer.param_groups[0]["lr"]
-            pbar.set_postfix(lr=lr, **summary_loss.avg)
-        loss_avg = summary_loss.avg
-        return loss_avg
+            pbar.set_postfix(lr=lr, **summaries.avg)
+        summaries_avg = summaries.avg
+        return summaries_avg
 
     @torch.inference_mode()
-    def validation_epoch(self, loader):
+    def validation_epoch(self, loader: DataLoader):
         self.model.eval()
-        summary_loss = MeterDict()
+        summaries = MeterDict()
         pbar = tqdm(enumerate(loader), total=len(loader))
         for step, batch in pbar:
-            loss = self.validation_step(batch, step)
+            result = self.validation_step(batch, step)
+            outputs, loss, targets = (
+                result["outputs"],
+                result["loss"],
+                result["targets"],
+            )
             # update stats
-            summary_loss.update("loss", loss.item())
-            pbar.set_postfix(**summary_loss.avg)
-        loss_avg = summary_loss.avg
-        return loss_avg
+            probs = torch.sigmoid(outputs)
+            stats = smp.metrics.get_stats(
+                probs, targets, mode="multilabel", threshold=0.5
+            )
+            iou_per_class: torch.Tensor = smp.metrics.iou_score(
+                *stats, reduction=None
+            ).mean(axis=0)
+            batch_size = targets.size(0)
+            summaries.update("loss", loss.item(), batch_size)
+            for cat, iou in zip(self.categories, iou_per_class):
+                summaries.update(f"{cat}_iou", iou.item(), batch_size)
+            summaries.update(f"macro_avg_iou", iou_per_class.mean().item(), batch_size)
+            pbar.set_postfix(**summaries.avg)
+        summaries_avg = summaries.avg
+        return summaries_avg
 
     def fit(
         self, data_module: HierTextDataModule, epochs: int, callbacks_: List[Callback]
@@ -121,9 +141,11 @@ class HierTextModelModule:
         for epoch in range(1, epochs + 1):
             callbacks.on_epoch_begin(epoch=epoch)
             logger.info("# Epoch {}/{}: #".format(epoch, epochs))
-            train_loss = self.training_epoch(train_loader)
-            val_loss = self.validation_epoch(val_loader)
-            callbacks.on_epoch_end(epoch=epoch, logs={"val_loss": val_loss["loss"]})
+            train_summaries = self.training_epoch(train_loader)
+            val_summaries = self.validation_epoch(val_loader)
+            callbacks.on_epoch_end(
+                epoch=epoch, logs={"val_loss": val_summaries["loss"]}
+            )
             if self.stop_training:
                 break
         callbacks.on_train_end()
